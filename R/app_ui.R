b40ae7579f432128ccb244ef71e713f9b9b75bac
caitobrien <csobrien@uw.edu>
2025-03-11 15:46:29 -0700

    add logo and header and beta versioning

--- R/app_ui.R
+++ R/app_ui.R
@@ -12,9 +12,20 @@ app_ui <- function(request) {
     fluidPage(
 
       ## Page content
-      shinydashboard::dashboardPage(
+      shinydashboardPlus::dashboardPage(
         header = shinydashboard::dashboardHeader(
-          title = "Columbia Basin Research" #Seasonal Predictions of Smolt-to-Adult Survival and the Transported to Bypassed fish survival ratio (T:B)
+          title = HTML("SARforecastDLM <small style ='font-size:0.6em;'>BetaVersion.Mar25</small>"),
+          # Add a logo to the header
+          tags$li(
+            class = "dropdown header-img",
+            tags$style(HTML("
+              .header-img {
+                float: right;
+                padding-right: 10px;
+              }
+            ")),
+            tags$img(src = "www/cbr_logo_horiz.png", height = "50px")
+          )
         ),
 
         ## Sidebar content - used as a navigation menu to each tab
@@ -43,6 +54,28 @@ app_ui <- function(request) {
             shinydashboard::tabItem(tabName = "about",mod_about_page_ui("about_page_ui_1")),
             shinydashboard::tabItem(tabName = "figs", mod_mainpage_ui("mainpage_1")),
             shinydashboard::tabItem(tabName = "bkg", mod_supplementary_page_ui("supplementary_page_1"))
+          )
+        ),
+        footer = shinydashboardPlus::dashboardFooter(
+          left = HTML(
+            '<div style="color: #024c63;">
+       <a href="https://cbr.washington.edu/" target="_blank" style="color: #024c63; text-decoration: none;" onmouseover="this.style.textDecoration=\'underline\';" onmouseout="this.style.textDecoration=\'none\';">Columbia Basin Research</a> •
+       <a href="https://safs.uw.edu/" target="_blank" style="color: #024c63; text-decoration: none;" onmouseover="this.style.textDecoration=\'underline\';" onmouseout="this.style.textDecoration=\'none\';">School of Aquatic and Fishery Sciences</a> •
+       <a href="https://environment.uw.edu/" target="_blank" style="color: #024c63; text-decoration: none;" onmouseover="this.style.textDecoration=\'underline\';" onmouseout="this.style.textDecoration=\'none\';">College of the Environment</a> •
+       <a href="https://www.washington.edu/" target="_blank" style="color: #024c63; text-decoration: none;" onmouseover="this.style.textDecoration=\'underline\';" onmouseout="this.style.textDecoration=\'none\';">University of Washington</a>
+     </div>'
+          ),
+          right = HTML(
+            '<span class="footer-contact" style="color: #024c63;">
+       <a href="mailto:web@cbr.washington.edu" target="_blank" style="color: #024c63; text-decoration: none;" onmouseover="this.style.textDecoration=\'underline\';" onmouseout="this.style.textDecoration=\'none\';">
+         <i class="fa fa-envelope" style="color: #024c63;"></i> web@cbr.washington.edu
+       </a>
+       &nbsp;&nbsp;
+       <a href="https://github.com/Columbia-Basin-Research-CBR/SARforecastDLM.git"
+          target="_blank" style="color: #024c63; text-decoration: none;" onmouseover="this.style.textDecoration=\'underline\';" onmouseout="this.style.textDecoration=\'none\';">
+         <i class="fa fa-github" style="color: #024c63;"></i> github.com/Columbia-Basin-Research-CBR
+       </a>
+     </span>'
           )
         )
       )
